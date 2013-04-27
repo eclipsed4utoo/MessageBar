6d2ab0735f82c5b3fdb4b8e8ddd2be1b86c1ba14
Ryan Alford <ryan@eclipsed4utoo.com>
2013-04-27 10:50:39 -0400

    initial commit

---
+++ MessageBar/MessageBar.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace DataArtist
+{
+    public partial class MessageBar : UserControl
+    {
+        private bool _show;
+        private DispatcherTimer Timer { get; set;}
+        
+        public MessageBar()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get
+            {
+                return MessageText.Text;
+            }
+            set
+            {
+                MessageText.Text = value;
+            }
+        }
+
+        public bool Show
+        {
+            get
+            {
+                return _show;
+            }
+            set
+            {
+                StopTimer();
+                GotoBarState(value);
+                _show = value;
+            }
+        }
+
+        public void TimeShow(int seconds)
+        {
+            InitTimer(seconds);
+            _show = true;
+            GotoBarState(_show);
+            Timer.Start();
+        }
+
+        private void GotoBarState(bool showBar)
+        {
+            VisualStateManager.GoToState(this, ((showBar) ? "ShowBar" : "HideBar"), true);
+        }
+
+        private void InitTimer(int seconds)
+        {
+            StopTimer();
+
+            Timer = new DispatcherTimer
+                        {
+                            Interval = TimeSpan.FromSeconds(seconds)
+                        };
+
+            Timer.Tick += (s, e) =>
+                        {
+                            StopTimer();
+                            Show = false;
+                        };
+        }
+
+        private void StopTimer()
+        {
+            if (Timer != null && Timer.IsEnabled == true)
+            {
+                Timer.Stop();
+            }
+        }
+    }
+}
---
+++ MessageBarDemo/MessageBarDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MessageBarDemo
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            myMsgBar.Text = "Record has been deleted. (just kidding!!!)";
+            myMsgBar.TimeShow(2);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            myMsgBar.Text = "Welcome to DataArtist MessageBar demo!";
+            myMsgBar.TimeShow(5);
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            myMsgBar.Text = "Showing some test data...";
+            myMsgBar.Show = true;
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            myMsgBar.Show = false;
+        }
+    }
+}
